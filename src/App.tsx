6b9c941e5059f1459e8f2e3ec186a4710af76ae2
Brian Ng <brianngchongeu@gmail.com>
2024-02-10 01:50:21 +1100

    feat(WIP): set up form submission
    - ensure all input fields are validated against new yup validation schema
    - prepare calculate term deposit function

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import {
   Card,
@@ -20,82 +20,139 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { calculateTermDeposit } from "@/lib/utils";
 
 function App() {
   const validateSchema = Yup.object().shape({
-    depositAmount: Yup.number().required().min(10000).max(500000).positive(),
-    interestRate: Yup.number().required().min(1.1).positive(),
-    investmentTerm: Yup.number().required().min(1).max(30).positive(),
+    depositAmount: Yup.number()
+      .moreThan(0, "The deposit amount must be at least $1.")
+      .required("Please enter your deposit amount."),
+    interestRate: Yup.number()
+      .moreThan(0, "The interest rate must be at least 0.01%.")
+      .required("Please enter your interest rate."),
+    investmentTerm: Yup.number()
+      .moreThan(0, "The investment term must be at least 1 year.")
+      .required("Please enter your investment term."),
     interestPaid: Yup.string()
-      .required()
+      .required(
+        "Please declare your interest paid monthly, quarterly, annually, or at maturity."
+      )
       .oneOf(["monthly", "quarterly", "annually", "at-maturity"]),
   });
 
   return (
-    <main className="flex items-center justify-center">
-      <Card className="w-[350px]">
-        <Formik
-          initialValues={{
-            depositAmount: 100000,
-            interestRate: 1.1,
-            investmentTerm: 3,
-            interestPaid: "monthly",
-          }}
-          validate={validateSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <CardHeader>
-                <CardTitle>Term deposit calculator</CardTitle>
-                <CardDescription>
-                  Calculate your term deposit below.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="depositAmount">Deposit amount</Label>
-                    <Input type="number" name="depositAmount" />
-                    <ErrorMessage name="depositAmount" component="div" />
+    <main className="flex h-screen items-center justify-center">
+      <section>
+        <Card className="w-[350px]">
+          <Formik
+            initialValues={{
+              depositAmount: 10000,
+              interestRate: 1.1,
+              investmentTerm: 3,
+              interestPaid: "monthly" as const,
+            }}
+            validationSchema={validateSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(calculateTermDeposit(values));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ values, errors, isSubmitting, setFieldValue }) => (
+              <Form>
+                <CardHeader>
+                  <CardTitle>Term deposit calculator</CardTitle>
+                  <CardDescription>
+                    Calculate your term deposit below.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="depositAmount">Deposit amount</Label>
+                      <Input
+                        type="number"
+                        name="depositAmount"
+                        step="any"
+                        min={1}
+                      />
+                      <ErrorMessage
+                        name="depositAmount"
+                        component="div"
+                        className="text-xs text-red-400"
+                      />
 
-                    <Label htmlFor="interestRate">Interest rate</Label>
-                    <Input type="number" name="interestRate" />
-                    <ErrorMessage name="interestRate" component="div" />
+                      <Label htmlFor="interestRate">Interest rate (%)</Label>
+                      <Input
+                        type="number"
+                        name="interestRate"
+                        step=".01"
+                        min={0.01}
+                      />
+                      <ErrorMessage
+                        name="interestRate"
+                        component="div"
+                        className="text-xs text-red-400"
+                      />
 
-                    <Label htmlFor="investmentTerm">Investment term</Label>
-                    <Input type="number" name="investmentTerm" />
-                    <ErrorMessage name="investmentTerm" component="div" />
+                      <Label htmlFor="investmentTerm">
+                        Investment term (years)
+                      </Label>
+                      <Input
+                        type="number"
+                        name="investmentTerm"
+                        step="1"
+                        min={1}
+                        max={30}
+                      />
+                      <ErrorMessage
+                        name="investmentTerm"
+                        component="div"
+                        className="text-xs text-red-400"
+                      />
 
-                    <Label htmlFor="interestPaid">Interest paid</Label>
-                    <Select name="interestPaid">
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="I would like my interest paid" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="monthly">Monthly</SelectItem>
-                        <SelectItem value="quarterly">Quarterly</SelectItem>
-                        <SelectItem value="annually">Annually</SelectItem>
-                        <SelectItem value="at-maturity">At Maturity</SelectItem>
-                      </SelectContent>
-                    </Select>
+                      <Label htmlFor="interestPaid">Interest paid</Label>
+                      <Select
+                        name="interestPaid"
+                        defaultValue={values.interestPaid}
+                        onValueChange={(value) => {
+                          setFieldValue("interestPaid", value);
+                        }}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="I would like my interest paid" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="monthly">Monthly</SelectItem>
+                          <SelectItem value="quarterly">Quarterly</SelectItem>
+                          <SelectItem value="annually">Annually</SelectItem>
+                          <SelectItem value="at-maturity">
+                            At Maturity
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <ErrorMessage
+                        name="interestPaid"
+                        component="div"
+                        className="text-xs text-red-400"
+                      />
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-              <CardFooter className="flex justify-between">
-                <Button type="submit" disabled={isSubmitting}>
-                  Calculate
-                </Button>
-              </CardFooter>
-            </Form>
-          )}
-        </Formik>
-      </Card>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  <Button
+                    type="submit"
+                    disabled={errors.length > 0 || isSubmitting}
+                  >
+                    Calculate
+                  </Button>
+                </CardFooter>
+              </Form>
+            )}
+          </Formik>
+        </Card>
+      </section>
     </main>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,20 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+type TermDepositFormSchema = {
+  depositAmount: number;
+  interestRate: number;
+  investmentTerm: number;
+  interestPaid: "monthly" | "quarterly" | "annually" | "at-maturity";
+};
+
 /**
- * Calculate the interest for a term deposit.
+ * Calculate term deposit.
+ *
+ * @param values
+ * @returns
  */
-export function calculateInterest() {}
+export function calculateTermDeposit(values: TermDepositFormSchema) {
+  const calculatedResult = values.depositAmount * values.interestRate;
+  return JSON.stringify({ result: calculatedResult, ...values }, null, 2);
+}
